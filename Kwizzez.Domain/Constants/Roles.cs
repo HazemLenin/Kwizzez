ed140428ad3db2acdbce9e74fb76fcd67c487f2b
Hazem Lenin <63303495+HazemLenin@users.noreply.github.com>
2023-07-23 19:55:20 +0300

    Add project files.

---
+++ Kwizzez.Domain/Constants/Roles.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kwizzez.Domain.Constants
+{
+    public static class Roles
+    {
+        public const string Admin = nameof(Admin);
+        public const string Teacher = nameof(Teacher);
+        public const string Student = nameof(Student);
+    }
+}
